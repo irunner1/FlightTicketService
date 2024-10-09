db3928896ceb26eba80cdfd00a309f2ae931c52f
Пучков Артём <aspuchkov02@gmail.com>
2024-10-09 16:33:34 +0300

    changed flights logic, added api methods

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -20,13 +20,20 @@ func main() {
 
 	host := os.Getenv("HOST")
 	port := os.Getenv("PORT")
+	log.Printf("loaded env `host: %s, port: %s`", host, port)
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/api/flights", GetFlights).Methods("GET")
-	r.HandleFunc("/api/flights/{id}", GetFlightInfo).Methods("GET")
-	r.HandleFunc("/api/tickets", BookTicket).Methods("POST")
-	r.HandleFunc("/api/checkin", CheckInOnline).Methods("POST")
+	// r.HandleFunc("/api/openapi.json", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, "openapi.json")
+	// }).Methods("GET")
+	// r.PathPrefix("/swagger/").Handler(http.StripPrefix("/swagger/", http.FileServer(http.Dir("path/to/swaggerui/"))))
+
+	r.HandleFunc("/api/v1/flights", GetFlights).Methods("GET")
+	r.HandleFunc("/api/v1/flights_by_params", GetFlightsByParams).Methods("GET")
+	r.HandleFunc("/api/v1/flights/{id}", GetFlightInfo).Methods("GET")
+	r.HandleFunc("/api/v1/tickets", BookTicket).Methods("POST")
+	r.HandleFunc("/api/v1/checkin", CheckInOnline).Methods("POST")
 
 	srv := &http.Server{
 		Handler:      r,
@@ -44,13 +51,46 @@ func main() {
 // GetFlights handles requests for getting list of flights.
 func GetFlights(w http.ResponseWriter, r *http.Request) {
 	flightSer := flights.NewFlightService()
-	searchParams := flights.SearchParams{
-		Origin:      "Moscow",
-		Destination: "Istanbul",
-		Departure:   time.Date(2024, time.April, 15, 10, 0, 0, 0, time.UTC),
-		Arrival:     time.Date(2024, time.April, 15, 13, 30, 0, 0, time.UTC),
+	flights, err := flightSer.GetFlights()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	flights, err := flightSer.GetFlights(searchParams)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(flights)
+}
+
+// GetFlightsByParams handles requests for getting list of flights by parameters.
+// GET /flights?origin=Moscow&destination=Istanbul&departure=2024-04-15T10:00:00Z&arrival=2024-04-15T13:30:00Z
+// GET /flights?origin=New%20York&destination=London&departure=2023-04-15T08:00:00Z&arrival=2023-04-15T20:00:00Z
+func GetFlightsByParams(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	origin := query.Get("origin")
+	destination := query.Get("destination")
+
+	departure, err := time.Parse(time.RFC3339, query.Get("departure"))
+	if err != nil {
+		http.Error(w, "Invalid departure time format. Use RFC3339.", http.StatusBadRequest)
+		return
+	}
+	arrival, err := time.Parse(time.RFC3339, query.Get("arrival"))
+	if err != nil {
+		http.Error(w, "Invalid arrival time format. Use RFC3339.", http.StatusBadRequest)
+		return
+	}
+
+	searchParams := flights.SearchParams{
+		Origin:      origin,
+		Destination: destination,
+		Departure:   departure,
+		Arrival:     arrival,
+	}
+
+	flightSer := flights.NewFlightService()
+	flights, err := flightSer.GetFlightsByParams(searchParams)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
